c2f7f69cfe83639e785e5ad6619ed37cdbcf74c4
mrudula-n <89772841+mrudula-n@users.noreply.github.com>
2023-12-11 15:55:41 +0530

    Test branch (#2)
    * created branch
    
    * added drawIo
    
    ---------
    
    Co-authored-by: mrudula-n <saimrudulanomula@msitprogram>

--- add.py
+++ add.py
@@ -18,3 +18,4 @@ if __name__ == "__main__":
     print(add(1, 2))    # Should output 3
     print(add(-1, 1))   # Should output 0
     print(add(5.5, 4.5)) # Should output 10.0
+    print(add(7.5, 4.5)) # Should output 12.0
