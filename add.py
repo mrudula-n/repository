0a4af5b11d0924fbec36368763adb1cf779cd10e
mrudula-n <89772841+mrudula-n@users.noreply.github.com>
2023-12-11 13:57:34 +0530

    created branch (#1)
    Co-authored-by: mrudula-n <saimrudulanomula@msitprogram>

---
+++ add.py
@@ -0,0 +1,20 @@
+def add(num1, num2):
+    """
+    This function takes two numbers, num1 and num2, as input and returns their sum.
+
+    Parameters:
+    num1 (int/float): The first number to add.
+    num2 (int/float): The second number to add.
+
+    Returns:
+    int/float: The sum of num1 and num2.
+    """
+    
+    # Replace the 'pass' statement with the addition logic
+    return num1 + num2
+
+# Test cases to validate the function
+if __name__ == "__main__":
+    print(add(1, 2))    # Should output 3
+    print(add(-1, 1))   # Should output 0
+    print(add(5.5, 4.5)) # Should output 10.0
